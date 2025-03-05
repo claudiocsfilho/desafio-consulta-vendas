63fc34db0c82af0df59c097276214052ec643e00
Claudio Filho <claudiocsfilho@gmail.com>
2025-03-05 19:08:11 -0300

    Add search summary by date

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO (obj.seller.name as sellerName, " +
+            "SUM(obj.amount) as total) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY obj.seller.name")
+    List<SaleSummaryDTO> searchSummary (LocalDate minDate, LocalDate maxDate);
 }
