91209d2fae679a7d9ae6829c4bd09684bfbc8354
Claudio Filho <claudiocsfilho@gmail.com>
2025-03-05 19:08:11 -0300

    Add search sumary by date

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
-import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -20,4 +23,12 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public List<SaleSummaryDTO> searchSummary (String minDate, String maxDate){
+		LocalDate finalDate = maxDate.isEmpty() ? LocalDate.now(ZoneId.systemDefault()) : LocalDate.parse(maxDate);
+		LocalDate startDate = minDate.isEmpty() ? finalDate.minusYears(1L) : LocalDate.parse(minDate);
+		return repository.searchSummary(startDate, finalDate);
+
+	}
+
 }
